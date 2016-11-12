fd8e99a6934a28b4e1670e62f8e0064fbdb9fea7
William Laeder <codylaeder@gmail.com>
2016-11-12 15:39:38 -0500

    Working

---
+++ src/lib.rs
@@ -0,0 +1,56 @@
+
+
+//!Make File Descriptors Nonblocking
+//!
+//!Creates a simple interface around fcntl to set file descriptors as non
+//!blocking. This crate will work on all `unix` systems as classified by
+//!Rust. 
+
+extern crate mio;
+use mio::unix::EventedFd;
+extern crate libc;
+use libc::{F_GETFD,F_SETFD,fcntl,O_NONBLOCK};
+use std::io::Error;
+use std::os::unix::io::{AsRawFd,RawFd};
+
+
+///Make File Descriptor NonBlocking
+///
+///Sets the O_NONBLOCK for more information [follow this
+///link](http://man7.org/linux/man-pages/man2/fcntl.2.html)
+///
+pub fn set_non_blocking<'a, F>(fd: &'a F) -> Result<(),Error>
+where F: AsRawFd
+{
+    let fd = fd.as_raw_fd();
+    unsafe{
+        let flags = fcntl(fd, F_GETFD, 0);
+        let status = fcntl(fd,F_SETFD, flags | O_NONBLOCK);
+        if status < 0 {
+            Err(Error::last_os_error())
+        } else {
+            Ok(())
+        }
+    }
+}
+
+///Make an Evented Fd
+///
+///Creates a simple and safe interface to interface with MIO. The goal is
+///to allow mio to work with File IO. Provided your system is unix this
+///will let it interface with MIO event loop.
+///
+///The `RawFd` passed to this function will be made non-blocking.
+///
+pub fn evented_fd<'a>(fd: &'a RawFd) -> Result<EventedFd<'a>,Error> {
+    let f_d = *fd;
+    unsafe{
+        let flags = fcntl(f_d, F_GETFD, 0);
+        let status = fcntl(f_d,F_SETFD, flags | O_NONBLOCK);
+        if status < 0 {
+            Err(Error::last_os_error())
+        } else {
+            Ok(EventedFd(fd))
+        }
+    }
+}
